12299f9d3db5f2e3016f569431b1f9500cef452c
HrytskivB <gritskivbl99@gmail.com>
2017-11-08 21:55:32 +0200

    BugFixed

--- src/applicationfolder/WindowMessage.java
+++ src/applicationfolder/WindowMessage.java
@@ -1,48 +1,14 @@
 package applicationfolder;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert;
 
 public class WindowMessage {
-    final public static void winAlert (String alert) {
-        Stage stage = new Stage();
-        Pane message = new Pane();
-        Text warning = new Text(alert);
-        warning.setTranslateX(50);
-        warning.setTranslateY(25);
-        Button exit = new Button("OK");
-        exit.setTranslateX(58);
-        exit.setTranslateY(40);
-        message.getChildren().addAll(exit, warning);
-        exit.setOnAction(event -> {
-            stage.close();
-        });
-        Scene scn = new Scene(message, 150, 100);
-        stage.setScene(scn);
-        stage.show();
-    }
+    final public static void winAlert(String alertText) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(alertText);
 
-    /*final public static String inputString () {
-        Stage stage = new Stage();
-        Pane message = new Pane();
-        TextField textField = new TextField();
-        String fileName = textField.getText();
-        textField.setTranslateX(50);
-        textField.setTranslateY(25);
-        Button submit = new Button("Submit");
-        submit.setTranslateX(58);
-        submit.setTranslateY(40);
-        message.getChildren().addAll(submit, textField);
-        submit.setOnAction(event -> {
-            stage.close();
-        });
-        Scene scn = new Scene(message, 150, 100);
-        stage.setScene(scn);
-        stage.show();
-        return fileName;
-    }*/
+        alert.showAndWait();
+    }
 }
