cf2622e0f2e994c5527c5995e951ce0297a270fd
Taras <swex99@gmail.com>
2018-03-13 21:45:25 +0200

    bb4

--- src/java/applicationfolder/Main.java
+++ src/java/applicationfolder/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 
         Image ico = new Image("/images/main_icon3.png");
         primaryStage.getIcons().add(ico);
+
     }
     public static void main(String[] args) { launch(args); }
 }
