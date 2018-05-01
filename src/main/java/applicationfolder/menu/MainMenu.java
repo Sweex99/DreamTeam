0b8238505abe32cd40be613aa29d52b8016da7e1
Taras <swex99@gmail.com>
2018-05-01 22:34:02 +0300

    CODE SMELL #3

--- src/main/java/applicationfolder/menu/MainMenu.java
+++ src/main/java/applicationfolder/menu/MainMenu.java
@@ -27,13 +27,19 @@ public class MainMenu {
     private static final String SETTING_LABEL_RED_STYLE = "-fx-border-color: red;-fx-text-fill: red;-fx-pref-width: 400px;-fx-pref-height: 40px;-fx-padding: 0 0 0 55px";
     private static final String SETTING_RED_LABEL = "-fx-border-width: 2px;-fx-border-color: white;-fx-pref-height: 250px;-fx-pref-width: 250px;";
     private static final String SETTING_LABEL_RED = "-fx-border-width: 2px;-fx-border-color: red;-fx-pref-height: 250px;-fx-pref-width: 250px;";
+    private static final String ERROR_LABEL_STYLE = "-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55";
     private static final String CONFIRM_SAVE_DATA = "Ваші дані збережені";
     private static final String REGISTRATION_LABEL_STYLE = "-fx-text-fill: #e2e2e2;-fx-font-size: 11pt;";
+    private static final String MAIN_FIELD_STYLE_CLASS = "field";
+    private static final String MAIN_PANE_STYLE_CLASS = "background";
+    private static final String PATH_ICON_LOGO  = "images/main_icon3.png";
+    private static final String PATH_STYLE_SHEETS = "/css/style.css";
 
 
-    public void setting(Stage primaryStage){
+
+    public void setting(Stage primaryStage) {
         StackPane root = new StackPane();
-        root.getStyleClass().add("background");
+        root.getStyleClass().add(MAIN_PANE_STYLE_CLASS);
 
         String[] splitNick;
         splitNick = dataBaseDriver.getNickname().split(" ");
@@ -57,21 +63,21 @@ public class MainMenu {
         Label youData = new Label("Ваші дані");
 
         PasswordField youPassword = new PasswordField();
-        youPassword.getStyleClass().add("field");
+        youPassword.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label youPass = new Label("Ваш пароль");
 
         PasswordField confirmPassword = new PasswordField();
-        confirmPassword.getStyleClass().add("field");
+        confirmPassword.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label youConfPass = new Label("Повторіть Пароль");
 
         Label changeSystemData = new Label("Системні дані");
 
         TextField youName = new TextField(splitNick[0]);
-        youName.getStyleClass().add("field");
+        youName.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label labelName = new Label("Ім'я");
 
         TextField youLName = new TextField(splitNick[1]);
-        youLName.getStyleClass().add("field");
+        youLName.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label labelLName = new Label("Прізвище");
 
         youName.setTranslateX(-200);
@@ -117,27 +123,24 @@ public class MainMenu {
             appearanceMenu(primaryStage);
         });
         save.setOnAction(event -> {
-            if(youName.getText().equals(splitNick[0]) && youLName.getText().equals(splitNick[1])){
-                if(youPassword.getText().equals("") && confirmPassword.getText().equals("")){
+            if (youName.getText().equals(splitNick[0]) && youLName.getText().equals(splitNick[1])) {
+                if (youPassword.getText().equals("") && confirmPassword.getText().equals("")) {
                     alertData.setText("Ви не ввели нові дані");
                     alertData.setStyle(SETTING_LABEL_RED_STYLE);
                     alertData.setVisible(true);
-                }
-                else{
+                } else {
                     dataBaseDriver.editPassword(confirmPassword.getText());
                     alertData.setText(CONFIRM_SAVE_DATA);
                     alertData.setStyle(SETTING_LABEL_GREEN_STYLE);
                     alertData.setVisible(true);
                 }
-            }
-            else {
+            } else {
                 dataBaseDriver.editNickname(youName.getText() + " " + youLName.getText());
-                if(youPassword.getText().equals("") && confirmPassword.getText().equals("")){
+                if (youPassword.getText().equals("") && confirmPassword.getText().equals("")) {
                     alertData.setText(CONFIRM_SAVE_DATA);
                     alertData.setStyle(SETTING_LABEL_GREEN_STYLE);
                     alertData.setVisible(true);
-                }
-                else{
+                } else {
                     dataBaseDriver.editPassword(confirmPassword.getText());
                     alertData.setText(CONFIRM_SAVE_DATA);
                     alertData.setStyle(SETTING_LABEL_GREEN_STYLE);
@@ -155,11 +158,7 @@ public class MainMenu {
             redLabelSystemData.setStyle(SETTING_LABEL_RED);
         });
 
-
-//        Scene scene = new Scene(root, 900, 600);
-
-
-        Image ico = new Image("images/main_icon3.png");
+        Image ico = new Image(PATH_ICON_LOGO);
         primaryStage.getIcons().add(ico);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Settings");
@@ -171,12 +170,12 @@ public class MainMenu {
         primaryStage.show();
 
 
-        scene.getStylesheets().add("/css/style.css");
+        scene.getStylesheets().add(PATH_STYLE_SHEETS);
     }
 
     private void registration(Stage primaryStage) {
         StackPane root = new StackPane();
-        root.getStyleClass().add("background");
+        root.getStyleClass().add(MAIN_PANE_STYLE_CLASS);
 
         Button back = new Button();
         back.getStyleClass().add("back");
@@ -195,22 +194,22 @@ public class MainMenu {
         thirdStep.getStyleClass().add("step3");
 
         TextField inputFName = new TextField();
-        inputFName.getStyleClass().add("field");
+        inputFName.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label firstName = new Label("Ваше Ім'я");
         firstName.setStyle(REGISTRATION_LABEL_STYLE);
 
         TextField inputLName = new TextField();
-        inputLName.getStyleClass().add("field");
+        inputLName.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label lastName = new Label("Прізвище");
         lastName.setStyle(REGISTRATION_LABEL_STYLE);
 
         TextField inputLogin = new TextField();
-        inputLogin.getStyleClass().add("field");
+        inputLogin.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label labelLogin = new Label("Логін");
         labelLogin.setStyle(REGISTRATION_LABEL_STYLE);
 
         PasswordField inputPassword = new PasswordField();
-        inputPassword.getStyleClass().add("field");
+        inputPassword.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label labelPassword = new Label("Пароль");
         labelPassword.setStyle(REGISTRATION_LABEL_STYLE);
 
@@ -220,7 +219,7 @@ public class MainMenu {
         next.setDefaultButton(true);
 
         Label errorsLabel = new Label();
-        errorsLabel.setStyle("-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55");
+        errorsLabel.setStyle(ERROR_LABEL_STYLE);
         errorsLabel.setVisible(false);
 
         Label secretQuestion = new Label("Секретне питання: ");
@@ -245,7 +244,7 @@ public class MainMenu {
         comboBox.setVisible(false);
 
         TextField answer = new TextField();
-        answer.getStyleClass().add("field");
+        answer.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         answer.setVisible(false);
 
         Button finishRegistration = new Button("Завершити Реєстрацію");
@@ -310,9 +309,7 @@ public class MainMenu {
         thirdStep.setTranslateY(-232);
         thirdStep.setTranslateX(310);
 
-        back.setOnAction(event -> {
-            authorization(primaryStage);
-        });
+        back.setOnAction(event -> authorization(primaryStage) );
         next.setOnMouseEntered(event -> {
             next.setStyle("-fx-background-color: #3a3a3a;");
         });
@@ -327,15 +324,14 @@ public class MainMenu {
         next.setOnAction(event -> {
             if ((inputFName.getText().trim().isEmpty()) || inputLName.getText().trim().isEmpty() || inputLogin.getText().trim().isEmpty() || inputPassword.getText().trim().isEmpty()) {
                 errorsLabel.setText("Деякі поля у формі не заповненні");
-                errorsLabel.setStyle("-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55");
+                errorsLabel.setStyle(ERROR_LABEL_STYLE);
                 errorsLabel.setVisible(true);
             } else {
-                if(dataBaseDriver.searchPerson(inputLogin.getText())){
+                if (dataBaseDriver.searchPerson(inputLogin.getText())) {
                     errorsLabel.setText("Логін який ви ввели вже існує");
-                    errorsLabel.setStyle("-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55");
+                    errorsLabel.setStyle(ERROR_LABEL_STYLE);
                     errorsLabel.setVisible(true);
-                }
-                else {
+                } else {
                     redLabelOne.setTranslateX(0);
 
                     errorsLabel.setText("Заповніть всі поля форми реєстрації");
@@ -359,7 +355,7 @@ public class MainMenu {
                     next.setOnAction(event1 -> {
                         if (answer.getText().trim().isEmpty() || comboBox.getValue() == "-Виберіть питання-") {
                             errorsLabel.setText("Деякі поля у формі не заповненні");
-                            errorsLabel.setStyle("-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55");
+                            errorsLabel.setStyle(ERROR_LABEL_STYLE);
                             errorsLabel.setVisible(true);
                         } else {
                             errorsLabel.setVisible(false);
@@ -391,8 +387,8 @@ public class MainMenu {
 
         Scene scene = new Scene(root, 900, 600);
 
-        scene.getStylesheets().add("/css/style.css");
-        Image ico = new Image("images/main_icon3.png");
+        scene.getStylesheets().add(PATH_STYLE_SHEETS);
+        Image ico = new Image(PATH_ICON_LOGO);
         primaryStage.getIcons().add(ico);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Registration");
@@ -405,15 +401,15 @@ public class MainMenu {
 
         StackPane root = new StackPane();
 
-        root.getStyleClass().add("background");
+        root.getStyleClass().add(MAIN_PANE_STYLE_CLASS);
 
         TextField loginField = new TextField();
         Label loginLabel = new Label("Логін: ");
-        loginField.getStyleClass().add("field");
+        loginField.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         loginLabel.setStyle(REGISTRATION_LABEL_STYLE);
 
         PasswordField passwordField = new PasswordField();
-        passwordField.getStyleClass().add("field");
+        passwordField.getStyleClass().add(MAIN_FIELD_STYLE_CLASS);
         Label passwordLabel = new Label("Пароль: ");
         passwordLabel.setStyle(REGISTRATION_LABEL_STYLE);
 
@@ -430,7 +426,7 @@ public class MainMenu {
         login.setStyle("-fx-text-fill: #e2e2e2;-fx-font-size: 20pt;");
 
         Label errorsLabel = new Label();
-        errorsLabel.setStyle("-fx-border-color: red;-fx-pref-height: 35px;-fx-pref-width: 300px;-fx-text-fill: red;-fx-padding: 0 0 0 55");
+        errorsLabel.setStyle(ERROR_LABEL_STYLE);
         errorsLabel.setVisible(false);
 
         loginField.setTranslateY(-40);
@@ -478,12 +474,12 @@ public class MainMenu {
             registration(primaryStage);
         });
 
-        root.getChildren().addAll( errorsLabel, login, loginButton, loginField, loginLabel, passwordField, passwordLabel, registration);
+        root.getChildren().addAll(errorsLabel, login, loginButton, loginField, loginLabel, passwordField, passwordLabel, registration);
 
         final Scene scene = new Scene(root, 900, 600);
 
-        scene.getStylesheets().add("/css/style.css");
-        Image ico = new Image("images/main_icon3.png");
+        scene.getStylesheets().add(PATH_STYLE_SHEETS);
+        Image ico = new Image(PATH_ICON_LOGO);
         primaryStage.getIcons().add(ico);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Log in");
@@ -494,7 +490,7 @@ public class MainMenu {
 
     public void appearanceMenu(Stage primaryStage) {
         StackPane root = new StackPane();
-        root.getStyleClass().add("background");
+        root.getStyleClass().add(MAIN_PANE_STYLE_CLASS);
 
         vBox.getStyleClass().add("accountInfo");
 
@@ -647,8 +643,8 @@ public class MainMenu {
 
         Scene scene = new Scene(root, 900, 600);
 
-        scene.getStylesheets().add("/css/style.css");
-        Image ico = new Image("images/main_icon3.png");
+        scene.getStylesheets().add(PATH_STYLE_SHEETS);
+        Image ico = new Image(PATH_ICON_LOGO);
         primaryStage.getIcons().add(ico);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Main menu");
